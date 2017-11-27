65622a4d6c9c9db364a16e9fdaac611f2223150d
Christopher Rodriguez <cdrodri1@uci.edu>
2017-11-27 15:58:47 -0800

    Update functions.js

--- JS/functions.js
+++ JS/functions.js
@@ -1,4 +1,10 @@
 //Javascript & jQuery
 //11-24-17
 
-const button
+const buttons = document.getElementByClassName("button");
+const displayscreen = document.querySelector("displayscreen > p")
+let value1 =""
+let value2 =""
+let operator=""
+
+let value="
