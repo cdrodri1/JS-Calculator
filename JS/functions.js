bad9493b377582ba71916fd8533703d0498937c3
Christopher Rodriguez <cdrodri1@uci.edu>
2017-11-26 22:13:41 -0800

    Update functions.js

--- JS/functions.js
+++ JS/functions.js
@@ -1,27 +1,4 @@
-//All Calculator Functions
+//Javascript & jQuery
 //11-24-17
 
-function add (x,y) {
-  return Number(x) + Number(y);
-};
-function subtract(x,y){
-  return Number(x) - Number(y);
-};
-function multiply(x,y){
-  return Number(x)*Number(y);
-};
-function divide(x,y){
-  return Number(x)/Number(y);
-};
-function operatordetec(operator, value1, value2){
-  switch (operator){
-    case "+":
-      return add(value1,value2);
-    case "-":
-      return subtract(value1,value2);
-    case "X":
-      return multiply(value1,value2);
-    case "/":
-      return divide(value1,value2);
-  }
-}
+const button
