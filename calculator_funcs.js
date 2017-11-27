80ea5a8081a456c0899a1bad7658cdfdd3e1c800
Christopher Rodriguez <cdrodri1@uci.edu>
2017-11-26 20:17:24 -0800

    Update calculator_funcs.js

--- calculator_funcs.js
+++ calculator_funcs.js
@@ -7,4 +7,21 @@ function add (x,y) {
 function subtract(x,y){
   return Number(x) - Number(y);
 };
-
+function multiply(x,y){
+  return Number(x)*Number(y);
+};
+function divide(x,y){
+  return Number(x)/Number(y);
+};
+function operatordetec(operator, value1, value2){
+  switch (operator){
+    case "+":
+      return add(value1,value2);
+    case "-":
+      return subtract(value1,value2);
+    case "X":
+      return multiply(value1,value2);
+    case "/":
+      return divide(value1,value2);
+  }
+}
