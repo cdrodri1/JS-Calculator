99a425cb0df9ec1db58b4a044d346b40de6ce89b
Christopher Rodriguez <cdrodri1@uci.edu>
2017-11-27 17:35:27 -0800

    Update functions.js

--- JS/functions.js
+++ JS/functions.js
@@ -2,9 +2,15 @@
 //11-24-17
 
 const buttons = document.getElementByClassName("button");
-const displayscreen = document.querySelector("displayscreen > p")
+const displayscreen = document.querySelector(".displayscreen > p")
 let value1 =""
 let value2 =""
 let operator=""
 
-let value="
+let value = function() {
+  for (var i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(event) {
+      let inner =event.target
+      if (inner.className=== "number"") {
+          if (value1.length <= 22) {
+        value1
