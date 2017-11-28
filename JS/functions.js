996b3007536babcd0349d70a5256fc2104d17c65
Christopher Rodriguez <cdrodri1@uci.edu>
2017-11-27 17:48:22 -0800

    Update functions.js

--- JS/functions.js
+++ JS/functions.js
@@ -11,6 +11,25 @@ let value = function() {
   for (var i=0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(event) {
       let inner =event.target
-      if (inner.className=== "number"") {
+      if (inner.className === "number") {
           if (value1.length <= 22) {
-        value1
+        value1 +=inner.innerHTML
+        screendisplay.innerHTML = value1;
+          }
+      } else if (inner.className === "operator") {
+        if (value2 ==="") {
+          value2 = value1
+          value1 =""
+          operator = inner.innerHTML
+        } else {
+          calc = calculate(operator, value1, value2)
+          operator = inner.innerHTML;
+          value1 = "";
+          displayscreen.innerHTML = calc;
+          value2 = calc;
+        }
+      } else if (["on", "clear"].includes(inner.className)) {
+        value1 ="";
+        value2 ="";
+        displayscreen.innerHTML = "MemCleared";
+        
